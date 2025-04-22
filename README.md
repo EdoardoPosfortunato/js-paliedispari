ab5a113dbe95b587c0849ab5d712261cbcdeefb7
EdoardoPosfortunato <edoardo.posfortunato27@gmail.com>
2025-04-22 17:35:33 +0200

    feat : esercizio Palindormo

---
+++ README.md
@@ -0,0 +1,22 @@
+## Spiegazione Esercizio
+
+Palidroma
+Chiedere all’utente di inserire una parola
+Creare una funzione per capire se la parola inserita è palindroma
+
+NON USARE SPLIT E REVERSE!!! TUTTO CON CICLO FOR
+
+## Uman Code
+
+1) ==Raccolta Dati==
+Chiedere all' utente una parola
+2) ==Esecuzione Logica==
+FUNZIONE: 
+- [x] creiamo una **FUNZIONE** in cui si va a ribaltare la parola 
+- [x] controlliamo che ogni lettera della stringa originale sia uguale alla stringa nuova
+- [x] RETURN -> treu / false
+
+3) ==Output==
+
+Se è vero allora è palindroma se è falso no.
+
---
+++ javascript/script.js
@@ -0,0 +1,34 @@
+// ESERCIZIO PALINDOROMO
+
+let stringa = prompt("Inserisci una Parola")
+// const stringa = "anna"
+
+let paolindromWorld = isPalindroma(stringa)
+
+if (paolindromWorld === false) {
+    console.log(`La parola ${stringa} non è palindorma`)
+} else if (paolindromWorld === true) {
+    console.log(`La parola ${stringa} è palindorma`)
+}
+
+////// FUNZIONI ////////
+
+function isPalindroma(parola) {
+
+    let parolaAlContrario = ""
+
+    for (let i = 0; i < parola.length; i++) {
+        parolaAlContrario = parolaAlContrario + parola[parola.length - 1 - i]
+    }
+
+    // console.log(parola)
+    // console.log(parolaAlContrario)
+
+    for (let i = 0; i < parola.length; i++) {
+        if (parola[i] !== parolaAlContrario[i]) {
+            return false
+        }
+    }
+    return true;
+
+}
